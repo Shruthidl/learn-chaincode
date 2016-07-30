e1ebc89cfeecbfeea8aca1ec8efe8501d8f62dab
A Walimbe <awalimbe@users.noreply.github.com>
2016-07-30 21:05:56 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -78,6 +78,9 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	} else if function == "write" {
 		return t.write(stub, args)
 	}
+	else if function == "addLoc" {
+		return t.addLoc(stub, args)
+	}
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation")
