1954cbbbd876a07121444e7f677c4d78c6fa89d9
A Walimbe <awalimbe@users.noreply.github.com>
2016-07-30 20:07:47 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -28,6 +28,7 @@ type SimpleChaincode struct {
 }
 
 var key string ;
+var value string ;
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
@@ -80,7 +81,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 
 // write - invoke function to write key/value pair
 func (t *SimpleChaincode) write(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	var value string
+	
 	var err error
 	fmt.Println("running write()")
 
