b17f96ecfd64eadf56d72460aa2a2801391a12a0
Shruthidl <shruthidl91@gmail.com>
2017-03-13 12:42:08 +0530

    Update chaincode_finished.go

--- gcsfinished/chaincode_finished.go
+++ gcsfinished/chaincode_finished.go
@@ -120,7 +120,7 @@ func (t *SimpleChaincode) addOutClearFile(stub shim.ChaincodeStubInterface, args
   
 	//prepareData
 	err := stub.PutState("364924",[]byte("|City Bank - 130"))
-	err := stub.PutState("364914",[]byte("|I Bank - 120"))
+	
 	if err != nil {
 		return nil, err
 	}
