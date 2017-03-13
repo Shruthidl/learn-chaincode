85d7ddd8f2c656c35708a5501c717d9268e0d497
Shruthidl <shruthidl91@gmail.com>
2017-03-13 12:32:49 +0530

    Update chaincode_finished.go

--- gcsfinished/chaincode_finished.go
+++ gcsfinished/chaincode_finished.go
@@ -75,9 +75,60 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
-	
+	// Handle different functions
+	if function == "read" { //read a variable
+		return t.read(stub, args)
+	} 
 	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query")
 }
+
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	
+	fmt.Println("saving state for key: " + key);
+	
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	
+	
+	return nil, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	
+	fmt.Println("retrieving state for key: " + key);
+	
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+        //valAsbytes = []byte(valAsbytes);
+	return valAsbytes, nil
+}
+
 
