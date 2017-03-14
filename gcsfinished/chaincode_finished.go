9db129f8a878f6bb43ba1583c577e3bb90b10c0a
Shruthidl <shruthidl91@gmail.com>
2017-03-14 16:57:56 +0530

    Update chaincode_finished.go

--- gcsfinished/chaincode_finished.go
+++ gcsfinished/chaincode_finished.go
@@ -172,8 +172,8 @@ func (t *SimpleChaincode) addOutClearFile(stub shim.ChaincodeStubInterface, args
 	
 	counter_s := strconv.Itoa(counter)
 	acq_name,err :=stub.GetState(args[0])
-	stringslice = append(stringslice,args[2],args[1],args[3],args[4],string(acq_name),"In Process");
-	stringvalues = append(stringslice,counter_s,counter_s)//string array (value)
+	stringslice = append(stringslice,counter_s,counter_s);
+	stringvalues = append(stringslice,args[2],args[1],args[3],args[4],string(acq_name),"In Process")//string array (value)
         s_requester := counter_s //counter value(key)
           	
 	     var mCount int = len(stringvalues);
