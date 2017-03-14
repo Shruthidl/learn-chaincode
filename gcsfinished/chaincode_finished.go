b4954a2e0787e5e66e2c5fd31169be9e5b16d534
Shruthidl <shruthidl91@gmail.com>
2017-03-14 15:01:38 +0530

    Update chaincode_finished.go

--- gcsfinished/chaincode_finished.go
+++ gcsfinished/chaincode_finished.go
@@ -193,7 +193,7 @@ func (t *SimpleChaincode) addOutClearFile(stub shim.ChaincodeStubInterface, args
 	}
 	
 	//precapture
-	 value,err :=stub.GetState(strconv.Itoa(counter))
+	/* value,err :=stub.GetState(strconv.Itoa(counter))
 		if err != nil {
 		return nil, err
 	}
@@ -229,7 +229,7 @@ func (t *SimpleChaincode) addOutClearFile(stub shim.ChaincodeStubInterface, args
 		err = stub.PutState(s_requester, []byte(stringBytes2));
 		
 		 return nil, nil;
-	}
+	}*/
 	
 	
               return nil, nil
