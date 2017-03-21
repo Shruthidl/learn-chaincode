0e7960b095cd9340478c80f9e10c80556c4c6e10
Shruthidl <shruthidl91@gmail.com>
2017-03-21 12:57:13 +0530

    Update chaincode_finished.go

--- gcsfinished/chaincode_finished.go
+++ gcsfinished/chaincode_finished.go
@@ -367,7 +367,8 @@ func (t *SimpleChaincode) markTxnCleared(stub shim.ChaincodeStubInterface, args 
 	 return nil,err	
 	}
  	 var str bytes.Buffer;
-		str.WriteString(string(valueAsBytes));
+		//str.WriteString(string(valueAsBytes));
+		str.WriteString("1|2|1240|364914020023481|123.00|Gloria Jeans-CH|4321432100|City Bank - 1301240|0.25|Validated|20-01-2017 07:20AM");
                 str.WriteString("|Cleared");
                 err = stub.PutState("t"+parts[j], []byte(str.String()));	
 	     
